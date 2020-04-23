d48b8890b8bb5814eda5d44091aee6d1c94f7902
Andrii Halabuda <andrii.halabuda@gmail.com>
2020-04-23 22:42:02 +0200

    Added Scrollable ImageView

--- Revamp/MainVC.swift
+++ Revamp/MainVC.swift
@@ -9,13 +9,32 @@
 import UIKit
 
 class MainVC: UIViewController {
+    
+    var imageScrollView: ImageScrollView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print(OpenCVWrapper.openCVVersion())
+        
+        imageScrollView = ImageScrollView(frame: view.bounds)
+        view.addSubview(imageScrollView)
+        view.sendSubviewToBack(imageScrollView)
+        setupImageScrollView()
+        
+//        let imagePath = Bundle.main.path(forResource: "autumn", ofType: "jpg")!
+        let image = UIImage(named: "IMG_7033")
+        
+        self.imageScrollView.set(image: image!)
     }
-
+    
+    func setupImageScrollView() {
+        imageScrollView.translatesAutoresizingMaskIntoConstraints = false
+        imageScrollView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        imageScrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        imageScrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        imageScrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+    }
 
 }
 
